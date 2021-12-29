a230f485ca51e1ff2684ccd8d2efcb53d5f12233
mici <60635066+midorimici@users.noreply.github.com>
2021-12-29 22:14:26 +0900

    🔧 Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,22 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 
-const clientConfig = {
-    mode: 'development',
-    entry: './src/client.ts',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
+module.exports = {
+  mode: 'development',
+  entry: './src/client.ts',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 };
-
-const serverConfig = {
-    mode: 'development',
-    entry: './svr/server.ts',
-    target: 'node',
-    node: {
-        __dirname: false,
-        __filename: false,
-    },
-    externals: [nodeExternals()],
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-}
-
-module.exports = [clientConfig, serverConfig];
